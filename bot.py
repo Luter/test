59e21110c217618e9db06e3bfaa4b5f0251d99b9
Luter <luter_pk@mail.ru>
2017-10-20 00:42:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import telebot
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+def repeat_all_messages(message): # ГЌГ Г§ГўГ Г­ГЁГҐ ГґГіГ­ГЄГ¶ГЁГЁ Г­ГҐ ГЁГЈГ°Г ГҐГІ Г­ГЁГЄГ ГЄГ®Г© Г°Г®Г«ГЁ, Гў ГЇГ°ГЁГ­Г¶ГЁГЇГҐ11
     bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
